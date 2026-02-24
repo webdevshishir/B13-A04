7be471f78f582c155313be0c453f497844596bbf
webdevshishihr <webdevshishir@gmail.com>
2026-02-24 07:35:38 +0600

    toggle Button

---
+++ script.js
@@ -0,0 +1,50 @@
+let interviewList = [];
+let rejectedList = [];
+
+
+
+// COunt
+
+
+let totalCount = document.getElementById('totalCount');
+let interviewCount = document.getElementById('interviewCount');
+let rejectedCount = document.getElementById('rejectedCount');
+
+const allFilterBtn = document.getElementById('all-filter-btn');
+const interviewFilterBtn = document.getElementById('interview-filter-btn');
+const rejectedFilterBtn = document.getElementById('rejected-filter-btn');
+
+
+const allCardSection = document.getElementById('allCards');
+
+const mainContainer = document.querySelector('main')
+console.log(mainContainer);
+
+console.log(allCardSection.children.length);
+
+function calculateCount(){
+    totalCount.innerText = allCardSection.children.length
+    interviewCount.innerText = interviewList.length
+    rejectedCount.innerText = rejectedList.length
+
+}
+calculateCount()
+  
+// Toggle Style
+
+function toggleStyle(id){
+    allFilterBtn.classList.remove('bg-blue-400','text-black')
+   interviewFilterBtn.classList.remove('bg-blue-400','text-black')
+    rejectedFilterBtn.classList.remove('bg-blue-400','text-black')
+
+     allFilterBtn.classList.add('bg-white','text-black')
+   interviewFilterBtn.classList.add('bg-white','text-black')
+    rejectedFilterBtn.classList.add('bg-white','text-black')
+
+    const selected = document.getElementById(id)
+    // console.log(selected);
+
+    selected.classList.remove('bg-white', 'text-black')
+    selected.classList.add('bg-blue-400', 'text-black')
+
+}
